61263c8cf7f3d829f95904aadfa9baf865b702e8
Tanjona <tanjona134@gmail.com>
2025-11-06 16:47:19 +0300

    Fix the following bugs (on the description)
    The first operand wasn't diplayed when a operator was clicked. Same
    for the result when a second or another operator was clicked.
    
    Pressing "=" before entering all of the numbers or an operator caused
    problems.
    
    Dividing by 0 is now stopping the operations when an operator is clicked
    and clears the display and all the data.

--- script.js
+++ script.js
@@ -1,91 +1,76 @@
-let num1, num2, operator, tempNum, result, isSecondReady, firstOperatorReady;
-isSecondReady = firstOperatorReady = false;
-
+let num1, num2, operator, tempNum, result;
+let isSecondNumReady, firstOperatorSet, shouldClearDisplay;
+isSecondNumReady = firstOperatorSet = shouldClearDisplay = false;
 
 let disp = document.querySelector(".display");
 
 let valToDisplay = document.createElement("p");
 
-console.log(num1);
+let clearBtn = document.querySelector(".clear-btn");
 
 displayDigits();
 
-
-
+clearBtn.addEventListener("click", clear);
 
 let ops = document.querySelectorAll(".operator");
 ops.forEach((op) => op.addEventListener("click", () => {
     let symbol = op.innerHTML;
     
-    
-    if(operator === undefined) {
+    if(operator === undefined && symbol !== "=") {
         operator = symbol;
         // console.log(`op: ${operator}`);
-        firstOperatorReady = true;
+        firstOperatorSet = true;
+        shouldClearDisplay = true;
     } 
     
-    else if (firstOperatorReady && symbol !== "=") {
+    else if (firstOperatorSet && symbol !== "=") {
         result = operate(operator, num1, num2);
         num1 = Math.round(result);
-        // console.log(num1);
+        num2 = undefined;
         operator = symbol;
+        shouldClearDisplay = true;
+
+        if(result === "Undefined") {
+            alert("undefined");
+            clear();
+        } else {
+            valToDisplay.textContent = num1;
+        }
+        
     }
 
-    isSecondReady = true;
-    valToDisplay.textContent = "";
+    isSecondNumReady = true;
+    // valToDisplay.textContent = "";
 
 
     
     if(symbol === "=" && num1 !== undefined && num2 !== undefined) {
         result = operate(operator, num1, num2);
 
-        //check whether the denomitor is 0 then throw undefined on the screen
-        if(result === "Undefined") valToDisplay.textContent = result;
-        else valToDisplay.textContent = Math.round(result);
+        //check whether the denominator is 0 then throw undefined on the screen
+        if(result === "Undefined") {
+            alert("undefined");
+            clear();
+        } else {
+            valToDisplay.textContent = Math.round(result);
+        }
+
+        shouldClearDisplay = true;
     }
+
+    console.log(`operator: ${operator}`)
 }));
 
-function add(num1, num2) {
-    return num1 + num2;
-}
-
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    if(num1 === 0) {
-        return 0;
-    } else if(num2 === 0) {
-        return "Undefined";
-    }
-
-    return num1 / num2;
-}
-
-function operate(operator, num1, num2) {
-    switch(operator) {
-        case "+": 
-            return add(num1, num2);
-        case "-": 
-            return subtract(num1, num2);
-        case "*": 
-            return multiply(num1, num2);
-        case "/": 
-            return divide(num1, num2);
-    }
-
-}
 
 function displayDigits() {
     let digits = document.querySelectorAll(".digit");
 
 
     digits.forEach((digit) => digit.addEventListener("click", () => {
+        if(shouldClearDisplay) {
+            valToDisplay.textContent = "";
+            shouldClearDisplay = false;
+        }
         
         switch(digit.innerHTML) {
             case "0":
@@ -122,7 +107,7 @@ function displayDigits() {
         
         tempNum = +valToDisplay.innerHTML;
 
-        if(isSecondReady) {
+        if(isSecondNumReady) {
             num2 = tempNum;
             // console.log(`num2: ${num2}`);
         } else { 
@@ -134,14 +119,46 @@ function displayDigits() {
     disp.appendChild(valToDisplay);
 }
 
-
-let clearBtn = document.querySelector(".clear-btn");
-
-clearBtn.addEventListener("click", () => {
+function clear() {
     num1 = num2 = operator = undefined; 
-    isSecondReady = false;
-    firstOperatorReady = false;
+    isSecondNumReady = false;
+    firstOperatorSet = false;
     valToDisplay.textContent = "";
-});
+}
 
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if(num1 === 0) {
+        return 0;
+    } else if(num2 === 0) {
+        return "Undefined";
+    }
+
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case "+": 
+            return add(num1, num2);
+        case "-": 
+            return subtract(num1, num2);
+        case "*": 
+            return multiply(num1, num2);
+        case "/": 
+            return divide(num1, num2);
+    }
+
+}
 
